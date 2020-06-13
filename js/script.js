a74389f6ccb6441fafc6971abf4179630665ef8d
Alexey-step <63848369+Alexey-step@users.noreply.github.com>
2020-06-13 16:18:16 +0500

    Скрипт

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var modal = document.querySelector('.modal-feedback');
 var button = document.querySelector('.location-contacts__feedback');
 var closeButton = document.querySelector('.feedback-content__close');
+var userName = document.querySelector('.modal-user_name');
+var userEmail = document.querySelector('.modal-user_email');
+var mainForm = document.querySelector('.main-form');
 var mapModal = document.querySelector('.map-modal');
 var mapButton = document.querySelector('.location-contacts__map');
 var closeMapButton = document.querySelector('.map-modal__close');
@@ -8,16 +11,60 @@ var cartModal = document.querySelector('.modal-cart');
 var cartButton = document.querySelectorAll('.content-cart');
 var closeCartButton = document.querySelector('.modal-cart__close');
 
+var isStorageSupport = true;
+var storage = "";
+
+try {
+    storage = localStorage.getItem('name');
+} catch (err) {
+    isStorageSupport = false;
+}
+
 if (button !== null) {
 button.addEventListener('click', function(evt) {
     evt.preventDefault();
     modal.classList.add('modal-feedback--show');
+
+    if (storage) {
+        userName.value = storage;
+        userEmail.focus();
+    } else {
+        userName.focus();
+    }
+
 });}
 
 if (closeButton !== null) {
-closeButton.addEventListener('click', function() {
+closeButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
     modal.classList.remove('modal-feedback--show');
+    modal.classList.remove('modal-feedback--error');
 });}
+
+if (mainForm !== null) {
+    mainForm.addEventListener('submit', function(evt) {
+        if (!userName.value || !userEmail.value) {
+        evt.preventDefault();
+        modal.classList.remove('modal-feedback--error');
+        modal.offsetWidth = modal.offsetWidth;
+        modal.classList.add('modal-feedback--error');
+      } else {
+        if (isStorageSupport) {
+          localStorage.setItem('name', userName.value);
+        }
+      }
+    })
+};
+
+window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+        if (modal.classList.contains('modal-feedback--show')) {
+            evt.preventDefault();
+            modal.classList.remove('modal-feedback--show');
+            modal.classList.remove('modal-feedback--error');
+        }
+    }
+});
 
 if (mapButton !== null) {
 mapButton.addEventListener('click', function(evt) {
